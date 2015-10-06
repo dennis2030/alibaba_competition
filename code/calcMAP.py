424e7954f1fcd28dc422b186693308c828ddef89
bean <dennis2030@gmail.com>
2015-10-07 00:27:26 +0800

    Add calcMap to output final MAP

---
+++ code/calcMAP.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+def __usage__():
+    print 'Usage: ./calcMAP.py query answer'
+
+def parseTopK(inputFile):
+    top_k_dict = {}
+
+    lines = open(inputFile, 'r').readlines()
+    for line in lines:
+        tmp_list = line.strip().split(',')
+        top_k_dict[tmp_list[0]] = []
+        
+        tmp_list2 = tmp_list[1].split(';')
+        for tmp in tmp_list2:
+            top_k_dict[tmp_list[0]].append(tmp)
+
+    return top_k_dict
+
+def calcAP(query, answer):
+    hit = 0
+    count = 0
+    P_sum = 0
+
+    for q in query:
+        count += 1
+        if q in answer:
+            hit += 1
+            P = float(hit)/count
+            P_sum += P
+    return P_sum/20.0
+
+def __main__():
+    
+    if( len(sys.argv) < 3 ):
+        __usage__()
+        return
+    
+    query = sys.argv[1]
+    answer = sys.argv[2]
+
+    query_dict = parseTopK(query)
+    answer_dict = parseTopK(answer)
+
+    ap_sum = 0
+    for key,value in query_dict.iteritems():
+        answer_list = answer_dict[key]
+        ap = calcAP(value, answer_list)
+        ap_sum += ap
+
+    print 'MAP is ' + str(ap_sum/500.0)
+
+
+
+if __name__ == '__main__':
+    __main__()
